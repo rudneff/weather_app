1218644b777dbfc195ec9d3a9156c5d0fae4be9b
rudneff <dmitryrudneff@gmail.com>
2016-09-29 19:31:52 +0300

    Some first approach.

---
+++ app/src/main/java/ru/tp/rudi/weather_app/ChooseCityActivity.java
@@ -0,0 +1,16 @@
+package ru.tp.rudi.weather_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import ru.mail.weather.lib.City;
+
+public class ChooseCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_city);
+    }
+}
