090a52b8a16df990d4c21f315c17f8f2ddb5ca78
rudneff <dmitryrudneff@gmail.com>
2016-10-02 05:01:08 +0300

    Works. Without saving.

--- app/src/main/java/ru/tp/rudi/weather_app/MainActivity.java
+++ app/src/main/java/ru/tp/rudi/weather_app/MainActivity.java
@@ -1,39 +1,38 @@
 package ru.tp.rudi.weather_app;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import ru.mail.weather.lib.City;
+import ru.mail.weather.lib.Weather;
 import ru.mail.weather.lib.WeatherStorage;
+import ru.mail.weather.lib.WeatherUtils;
 
-public class MainActivity extends AppCompatActivity implements ServiceHelper.WeatherChangeResultListener {
+public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver receiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button cityBtn = (Button)findViewById(R.id.choose_city_btn);
-        WeatherStorage weatherStorage = WeatherStorage.getInstance(this);
-        cityBtn.setText(weatherStorage.getCurrentCity().name());
-
-        cityBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startChooseCityActivity();
-            }
-        });
-
-        TextView weatherText = (TextView) findViewById(R.id.weather_text);
-
         Button backgroundUpdateOnBtn = (Button) findViewById(R.id.background_update_on_btn);
 
         backgroundUpdateOnBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeatherIntentService.class);
+                intent.setAction(WeatherIntentService.WEATHER_LOAD_ACTION);
+                WeatherUtils.getInstance().schedule(MainActivity.this, intent);
             }
         });
 
@@ -42,12 +41,72 @@ public class MainActivity extends AppCompatActivity implements ServiceHelper.Wea
         backgroundUpdateOffBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, WeatherIntentService.class);
+                intent.setAction(WeatherIntentService.WEATHER_LOAD_ACTION);
+                WeatherUtils.getInstance().unschedule(MainActivity.this, intent);
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        WeatherStorage weatherStorage = WeatherStorage.getInstance(this);
+        Button cityBtn = (Button)findViewById(R.id.choose_city_btn);
+        cityBtn.setText(weatherStorage.getCurrentCity().name());
+        cityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startChooseCityActivity();
+            }
+        });
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent != null && intent.getAction()
+                        .equals(WeatherIntentService.WEATHER_CHANGED_ACTION)) {
+                    MainActivity.this.updateWeather();
+                }
+            }
+        };
+
+        IntentFilter filter = new IntentFilter(WeatherIntentService.WEATHER_CHANGED_ACTION);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter);
+
+        Intent weatherLoadIntent = new Intent(MainActivity.this, WeatherIntentService.class);
+        weatherLoadIntent.setAction(WeatherIntentService.WEATHER_LOAD_ACTION);
+        startService(weatherLoadIntent);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (receiver != null) {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+            receiver = null;
+        }
 
     }
+
+    public void updateWeather() {
+        WeatherStorage weatherStorage = WeatherStorage.getInstance(this);
+        City currentCity = weatherStorage.getCurrentCity();
+        Weather weather = weatherStorage.getLastSavedWeather(currentCity);
+        TextView weatherTextView = (TextView) findViewById(R.id.weather_text);
+        if (weather != null) {
+            weatherTextView.setText(String.format("%s - %d (%s)", new Object[]{currentCity.name(),
+                    Integer.valueOf(weather.getTemperature()),
+                    weather.getDescription()}));
+        }
+        else {
+            weatherTextView.setText(R.string.error_message);
+        }
+    }
+
 
     private void startChooseCityActivity() {
         Intent intent = new Intent(this, ChooseCityActivity.class);
--- app/src/main/java/ru/tp/rudi/weather_app/WeatherIntentService.java
+++ app/src/main/java/ru/tp/rudi/weather_app/WeatherIntentService.java
@@ -2,73 +2,41 @@ package ru.tp.rudi.weather_app;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.content.Context;
-import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+
+import java.io.IOException;
+
+import ru.mail.weather.lib.City;
+import ru.mail.weather.lib.Weather;
+import ru.mail.weather.lib.WeatherStorage;
+import ru.mail.weather.lib.WeatherUtils;
 
 public class WeatherIntentService extends IntentService {
     public static final String WEATHER_ERROR_ACTION = "ru.tp.rudi.weather_app.WEATHER_ERROR_ACTION";
-    public static final String WEATHER_CHANGE_ACTION = "ru.tp.rudi.weather_app.WEATHER_CHANGE_ACTION";
-
+    public static final String WEATHER_CHANGED_ACTION = "ru.tp.rudi.weather_app.WEATHER_CHANGED_ACTION";
     public static final String WEATHER_LOAD_ACTION = "ru.tp.rudi.weather_app.WEATHER_LOAD_ACTION";
 
     public WeatherIntentService() {
         super("WeatherIntentService");
     }
 
-
-
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, WeatherIntentService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, WeatherIntentService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.d("!@!", "onHandleIntent");
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(this);
+        if (intent != null && intent.getAction().equals(WEATHER_LOAD_ACTION)) {
+            try {
+                City currentCity = WeatherStorage.getInstance(this).getCurrentCity();
+                Weather weather = WeatherUtils.getInstance().loadWeather(currentCity);
+                WeatherStorage.getInstance(this).saveWeather(currentCity, weather);
+                broadcastManager.sendBroadcast(new Intent(WEATHER_CHANGED_ACTION));
+            } catch (IOException e) {
+                e.printStackTrace();
+                broadcastManager.sendBroadcast(new Intent(WEATHER_ERROR_ACTION));
+            }
+        }
 
-    }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
